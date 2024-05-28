12d427e28a107f2d17ace3fc5dc9785cf21dbd21
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-05-28 21:43:32 +0600

    get the quantity and revenue

--- index.js
+++ index.js
@@ -216,14 +216,14 @@ async function run() {
       });
     })
 
-    app.get("/payments/:email",verifyToken, async(req, res) => {
-        const email = req.params.email
-        const query = {email: email}
-        if(email !== req.decoded.email){
-          return res.status(403).send({ message: "forbidden access" })
-        }
-        const result = await paymentCollection.find(query).toArray()
-        res.send(result)
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const email = req.params.email
+      const query = { email: email }
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      const result = await paymentCollection.find(query).toArray()
+      res.send(result)
     })
 
     app.post("/payments", async (req, res) => {
@@ -236,13 +236,71 @@ async function run() {
           }
         };
         const deleteResult = await cartCollection.deleteMany(query);
-    
+
         res.send({ paymentResult, deleteResult });
       } catch (error) {
         res.status(500).send({ message: "An error occurred while processing the payment.", error: error.message });
       }
     });
 
+    app.get('/admin-stats', async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalRevenue: {
+              $sum: "$price"
+            }
+
+          }
+        }
+      ]).toArray()
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0
+      res.send({ users, menuItems, orders, revenue })
+    })
+
+    app.get("/order-stats", async (req, res) => {
+      const result = await paymentCollection.aggregate([
+        {
+          $unwind: "$menuIds"
+        },
+        {
+          $addFields: {
+            menuIds: { $toObjectId: "$menuIds" }
+          }
+        },
+        {
+          $lookup: {
+            from: "menu",
+            localField: "menuIds",
+            foreignField: "_id",
+            as: "menuItems"
+          }
+        },
+        {
+          $unwind: "$menuItems"
+        },
+        {
+          $group: {
+            _id: "$menuItems.category",
+            quantity: { $sum: 1 },
+            revenue: { $sum: "$menuItems.price" }
+          }
+        },
+        {
+          $project: {
+            _id:0,
+            category: "$_id",
+            quantity: "$quantity",
+            revenue: "$revenue"
+          }
+        }
+      ]).toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
